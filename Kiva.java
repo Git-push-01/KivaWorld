52f58e33b32d2c08c2970cea807080a412157db5
Hugo Escandon <hescandon6@gmail.com>
2021-10-20 15:02:27 -0400

    update

--- Kiva.java
+++ Kiva.java
@@ -5,15 +5,24 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
+import edu.duke.FileResource;
 import edu.duke.Point;
 public class Kiva {
     
+ 
+FileResource fileResource = new FileResource();
+String inputMap = fileResource.asString();
 
 
-
+ 
+        
+        FloorMap floorMap = new FloorMap(inputMap);
+       
     Kiva kiva = new Kiva(); 
     
     public Kiva(){
+        
+ 
         
         
     }
